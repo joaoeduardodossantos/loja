e96b843167ec7d3aa7069c971b839dff271c53e4
joaoeduardo <santos.joaoeduardo@outlook.com>
2020-04-17 22:37:27 -0300

    Metodo Delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-	get "produtos/:id/remover", to: "produtos#destroy"
+	delete "produtos/:id", to: "produtos#destroy", as: :produto
 	post "produtos", to: "produtos#create"
 	get "produtos/new", to: "produtos#new"
 	root to: "produtos#index"
