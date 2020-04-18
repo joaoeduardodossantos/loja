afcae2014a0c3386b5ac3611dfcca695a759b81c
joaoeduardo <santos.joaoeduardo@outlook.com>
2020-04-17 23:03:05 -0300

    Rotas do Projetos usando Resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-	delete "produtos/:id", to: "produtos#destroy", as: :produto
-	post "produtos", to: "produtos#create"
-	get "produtos/new", to: "produtos#new"
+	#delete "produtos/:id", to: "produtos#destroy", as: :produto
+	#post "produtos", to: "produtos#create"
+	#get "produtos/new", to: "produtos#new"
+	resources :produtos, only: [:new, :create, :destroy]
 	root to: "produtos#index"
 end
