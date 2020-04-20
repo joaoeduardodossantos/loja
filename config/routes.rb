c5df43c0b336c00ccf01fdef0234ae5b47670244
joaoeduardo <santos.joaoeduardo@outlook.com>
2020-04-20 16:02:00 -0300

    Metodo busca - 20/04/2020

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 	#delete "produtos/:id", to: "produtos#destroy", as: :produto
 	#post "produtos", to: "produtos#create"
 	#get "produtos/new", to: "produtos#new"
-	get "produto/busca", to: "produto#busca", as: :busca_produto
+	get "produto/busca", to: "produtos#busca", as: :busca_produto
 	resources :produtos, only: [:new, :create, :destroy]
 	root to: "produtos#index"
 end
